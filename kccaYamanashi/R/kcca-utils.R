21650367fd1217d1ca252e05f1988f35e8a702d1
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-06 18:00:38 -0800

    split mkcca specific and general functions

---
+++ kccaYamanashi/R/kcca-utils.R
@@ -0,0 +1,41 @@
+
+################################################################################
+# Utilities used in both multiple and integrated kernel cca approaches.
+################################################################################
+
+#' @title Merge default options for kernel cca
+#' @param opts A partially specified list of options to use in kcca. The current
+#' possible options are
+#'   $kernels A list of kernlab kernel class objects. Defaults to a list of RBF
+#'    kernels with bandwidth 1.
+#'   $lambdas The regularization parameters for each table. Defaults to 1.
+#' @param p The number of tables in the kernel CCA.
+#' @return The original opts, but with unspecified options filled in with
+#' defaults.
+#' @importFrom kernlab rbfdot
+#' @export
+merge_kernel_opts <- function(opts = list(), p) {
+  default_opts <- list()
+  default_opts$kernels <- replicate(p, rbfdot(1), simplify = F)
+  default_opts$lambdas <- rep(1, p)
+  modifyList(default_opts, opts)
+}
+
+#' @title Get the kernel matrices associated with each table.
+#' @param x_list A list whose elements are matrices with samples on rows and
+#' features on columns.
+#' @param opts A list of options specifying how to perform the kcca. See
+#' merge_kernel_opts.
+#' @return A list whose i^th element is the n x n kernel matrix associated with
+#' the i^th data set.
+#' @importFrom kernlab kernelMatrix
+#' @export
+get_k_matrices <- function(x_list, opts) {
+  p <- length(x_list)
+  k_list <- vector(length = p, mode = "list")
+  for(i in seq_along(k_list)) {
+    k_list[[i]] <- kernelMatrix(opts$kernels[[i]], x_list[[i]])
+  }
+  k_list
+}
+
--- kccaYamanashi/R/mkcca.R
+++ kccaYamanashi/R/mkcca.R
@@ -1,47 +1,9 @@
 
 ################################################################################
-# Implementation of kernel cca method from "Extraction of correlated gene
-# clusters from multiple genomic data by generalized kernel canonical
-# correlation analysis." This is very similar to the kcca method in kernlab,
-# except it let's us work with more than two tables at a time.
+# Implementation of multiple kernel cca method from "Extraction of correlated
+# gene clusters from multiple genomic data by generalized kernel canonical
+# correlation analysis."
 ################################################################################
-
-
-#' @title Merge default options for kernel cca
-#' @param opts A partially specified list of options to use in kcca. The current
-#' possible options are
-#'   $kernels A list of kernlab kernel class objects. Defaults to a list of RBF
-#'    kernels with bandwidth 1.
-#'   $lambdas The regularization parameters for each table. Defaults to 1.
-#' @param p The number of tables in the kernel CCA.
-#' @return The original opts, but with unspecified options filled in with
-#' defaults.
-#' @importFrom kernlab rbfdot
-#' @export
-merge_kernel_opts <- function(opts = list(), p) {
-  default_opts <- list()
-  default_opts$kernels <- replicate(p, rbfdot(1), simplify = F)
-  default_opts$lambdas <- rep(1, p)
-  modifyList(default_opts, opts)
-}
-
-#' @title Get the kernel matrices associated with each table.
-#' @param x_list A list whose elements are matrices with samples on rows and
-#' features on columns.
-#' @param opts A list of options specifying how to perform the kcca. See
-#' merge_kernel_opts.
-#' @return A list whose i^th element is the n x n kernel matrix associated with
-#' the i^th data set.
-#' @importFrom kernlab kernelMatrix
-#' @export
-get_k_matrices <- function(x_list, opts) {
-  p <- length(x_list)
-  k_list <- vector(length = p, mode = "list")
-  for(i in seq_along(k_list)) {
-    k_list[[i]] <- kernelMatrix(opts$kernels[[i]], x_list[[i]])
-  }
-  k_list
-}
 
 #' @title Get the left hand side matrix in equation (16) of Yamanashi's kcca
 #' method
