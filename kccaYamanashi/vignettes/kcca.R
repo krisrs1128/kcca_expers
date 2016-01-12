fdbc0fad9335f996aaab7a37f009dae584ce7a70
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-11 22:36:57 -0800

    simulated data example

---
+++ kccaYamanashi/vignettes/kcca.R
@@ -0,0 +1,59 @@
+
+################################################################################
+# R script accompanying kcca.rnw
+################################################################################
+
+## ---- libraries ----
+library("ggplot2")
+library("kccaYamanashi")
+library("kernlab")
+library("reshape2")
+
+## ---- simulate-data ----
+n <- 40
+C <- sapply(1:2, function(x) sample(0:1, n, replace = T))
+theta <- C + matrix(runif(n * 2, 0, 1), n, 2)
+theta <- theta / rowSums(theta)
+
+p <- 100
+x <- seq(-4, 4, length.out = p)
+g <- list(dnorm(x, -1), dnorm(x, 1))
+
+f <- t(apply(theta, 1, function(z) z[1] * g[[1]] + z[2] * g[[2]]))
+
+## ---- vis-simulated-data ----
+mf <- melt(f)
+mf$x <- x[mf$Var2]
+mf$theta <- theta[, 1]
+ggplot(mf) +
+  geom_line(aes(x = x, y = value, group = Var1, col = theta))
+
+## ---- run-kcca ----
+sigma <- 3
+kernels <- list(list(rbfdot(sigma)), list(vanilladot()))
+kcca_res <- kcca(list(f, theta), list(kernels = kernels))
+
+## ---- vis-results ----
+ggplot(data.frame(ix = 1:10, lambda = kcca_res$values[1:10])) +
+  geom_bar(aes(x = ix, y = lambda), stat = "identity") +
+  ggtitle("top eigenvalues")
+
+u <- kcca_res$scores[[2]][, 1:2]
+ggplot(data.frame(theta = theta[, 1], u = u)) +
+  geom_point(aes(x = u.1, y = u.2, col = theta)) +
+  ggtitle("kcca scores")
+
+ggplot(data.frame(theta = theta[, 1], ux = u[1:n], uy = u[(n + 1):(2 * n)])) +
+  geom_point(aes(x = ux, y = uy)) +
+  ggtitle("Correlation between two table scores")
+
+v <- kcca_res$vectors[, 1]
+ggplot(data.frame(ix = 1:length(v), v, theta = theta[, 1])) +
+  geom_point(aes(x = ix, y = v, col = theta)) +
+  ggtitle("top eigenvector")
+
+v <- kcca_res$vectors[, 2]
+ggplot(data.frame(ix = 1:length(v), v, theta = theta[, 1])) +
+  geom_point(aes(x = ix, y = v, col = theta)) +
+  ggtitle("second eigenvector")
+
